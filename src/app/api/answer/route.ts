f4e340c7d69c217b38a504b9d7a54a304303de36
Jain4556 <sunil925business@gmail.com>
2026-02-20 21:29:10 +0530

    api_routeadded

---
+++ src/app/api/answer/route.ts
@@ -0,0 +1,89 @@
+import { databases } from "@/models/client/config";
+import { answerCollection, db } from "@/models/name";
+import { ID } from "appwrite";
+import { NextRequest, NextResponse } from "next/server";
+import { UserPrefs } from "@/store/Auth";
+import { users } from "@/models/server/config";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { questionId, answer, authorId } = await request.json()
+
+
+        const response = await databases.createDocument(db, answerCollection, ID.unique(), {
+            content: answer,
+            authorId: authorId,
+            questionId: questionId
+        })
+
+
+        //   Increase author reputation
+        const prefs = await users.getPrefs<UserPrefs>(authorId)
+
+        await users.updatePrefs(authorId, {
+            reputation: Number(prefs.reputation) + 1
+        })
+
+        return NextResponse.json(response, {
+            status: 201
+        })
+
+
+
+
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                error: error?.message || "error creating answer"
+
+            },
+            {
+
+                status: error?.status || error?.code || 500
+            }
+        )
+
+    }
+
+}
+
+
+export async function DELETE(request: NextRequest) {
+    try {
+
+     const {answerId} = await   request.json()
+    const answer =  await databases.getDocument(db, answerCollection, answerId)
+
+   const response =   await databases.deleteDocument(db, answerCollection, answerId)
+
+
+//    decrease the reputation
+
+  const prefs = await users.getPrefs<UserPrefs>(answer.authorId)
+
+        await users.updatePrefs(answer.authorId, {
+            reputation: Number(prefs.reputation) - 1
+        })
+
+        return NextResponse.json( 
+            {data:response},
+            {
+            status: 200}
+        )
+
+
+        
+    } catch (error: any) {
+          return NextResponse.json(
+            {
+               message: error?.message || "error deleting the answer"
+
+            },
+            {
+
+                status: error?.status || error?.code || 500
+            }
+        )
+    }
+    
+}
